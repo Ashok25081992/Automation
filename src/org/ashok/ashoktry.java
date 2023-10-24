9bc3045179a8d3bcb0a499b10f8a3584d091ce99
Ashok25081992 <b.ashokrajudece@gmail.com>
2023-10-24 12:35:20 +0530

    First Commit

---
+++ src/org/ashok/ashoktry.java
@@ -0,0 +1,13 @@
+package org.ashok;
+
+public class ashoktry {
+
+		private void StudentName() {
+			System.out.println("jack");
+		
+	}
+public static void main(String[] args) {
+	ashoktry at = new ashoktry();
+	at.StudentName();
+}
+}
